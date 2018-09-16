7ed268bcd34d5c15aed7321cee0188e02e590890
Rishi0721 <rishmikalavandi@gmail.com>
2018-09-17 02:57:32 +0530

    changes to contact

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,78 +1,28 @@
 @extends('layouts.app')
-
+<link rel="stylesheet" href="{{asset('my/l.css')}}">
 @section('content')
-<img src="img/car.jpg" width="1400px" height="300px">
-    <h1><img src="img/ic.png" width="70px" >
-        <img src="img/contact.gif" width="200px" ></h1>
+<img src="img/p1.jpg" width="1400px" height="300px">
+    <img src="img/ic.png" width="70px" >
+      <h1><img src="img/contact.gif" width="200px" ></h1>
     <br><br><br>
-
     
-<style>
-body {font-family: Arial, Helvetica, sans-serif;align: center}
-* {box-sizing: border-box;}
-.form-container {
-  
-    text-align:center;
-    align:center;
-  max-width: 600px;
-  padding: 40px;
-  background-color: lightslategray;
-}
-  
-.form-container input[type=text], select ,textarea {
-  width: 100%;
-  padding: 15px;
-  margin: 5px 0 22px 0;
-  border: none;
-  background: #f1f1f1;
-  margin-bottom:10px;
-}
-
-/* When the inputs get focus, do something */
-.form-container input[type=text],select,textarea {
-  background-color: white;
-  outline: none;
-}
-        
-    input[type=submit] {
+    <div class="container">
+        <form action="/contact.blade.php" class="form-container border border-warning rounded" style="width:600px">
+               <h1 style="color:goldenrod">LET'S GET IN TOUCH!</h1>
+               <p>Feel free to contact us for more information</p>
+                        <label for="name" style="color:goldenrod"><p style="text-align:left">Name : </label>
+                        <input type="text" class="form-control form-control-lg text-white bg-dark border border-warning rounded" id="name" placeholder="Name">
+                        <label for="contact" style="color:goldenrod">Contact No : </label>
+                        <input type="text" class="form-control form-control-lg text-white bg-dark border border-warning rounded" id="contact" placeholder="Phone number">
+                        <label for="email" style="color:goldenrod">Email : </label>
+                        <input type="text" class="form-control form-control-lg text-white bg-dark border border-warning rounded" id="email" placeholder="Email">
+                        <label for="address" style="color:goldenrod">Address : </label>
+                        <input type="text" class="form-control form-control-lg text-white bg-dark border border-warning rounded" id="address" placeholder="Address">
+                        <label for="subject" style="color:goldenrod">Subject :</label>
+                        <textarea class="form-control form-control-lg text-white bg-dark border border-warning rounded" id="subject"  placeholder="Write something.." ></textarea><br>
+                        <button type="submit" class="btn btn-warning float-right" >Submit</button>
+        </form>
+    </div>
+@endsection
             
-                
-    background-color: #4CAF50;
-    color: white;
-    padding: 12px 20px;
-    border: none;
-    border-radius: 4px;
-    margin-top: 20px;
-    cursor: pointer;
-    }
             
-            input[type=submit]:hover {
-                background-color: greenyellow;
-            }
-            
-            </style>
-            </head>
-            <body>
-            
-                
-        
-
-  <form action="/contact.blade.php" class="form-container">
-    <label for="fname">First Name</label>
-    <input type="text" id="fname" name="firstname" placeholder=" ">
-
-    <label for="lname">Last Name</label>
-    <input type="text" id="lname" name="lastname" placeholder=" ">
-
-    <label for="tele">Contact No</label>
-    <input type="text" id="contct" name="contact" placeholder=" ">
-
-    <label for="subject">Subject</label>
-    <textarea id="subject" name="subject" placeholder="Write something.." ></textarea>
-
-    <input type="submit" value="Submit">
-  </form>
-
-  
-    
-</body>
