c636cb274385768ecae79802439d051e96ba8c23
Thiranja <lakrandikathiranja@gmail.com>
2018-09-16 22:20:14 +0530

    Authrification added but still there are some problems with navbar and also login and register pages styles

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,27 +1,58 @@
-<nav class="navbar navbar-expand-sm bg-transparent navbar-dark fixed-top" >
-        <!-- Brand/logo -->
-        <a class="navbar-brand" href="/">
-          <img src="img/ic.png" alt="logo" style="width:70px;">
-        </a>
-        
-        <!-- Links -->
-        <ul class="navbar-nav" style="font-size:larger">
-          <li class="nav-item">
-            <a class="nav-link" href="/">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/service">Services</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/contact">Contact Us</a>
-          </li>
-        </ul>
-        <ul class="navbar-nav ml-auto" style="font-size:larger">
-                <li class="nav-item">
-                    <a class="nav-link" href="/login" ><img src="img\icons8_Login_35px.png" >Login</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#"><img src="img\icons8_Add_User_Male_35px.png" >SignUp</a>
-                </li>
+<nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+    <div class="container">
+      <!-- Brand/logo -->
+      <a class="navbar-brand" href="/">
+        <img src="img/ic.png" alt="logo" style="width:70px;">
+      </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav" style="font-size:larger">
+              <li class="nav-item">
+                <a class="nav-link" href="/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/service">Services</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/contact">Contact Us</a>
+              </li>
             </ul>
-      </nav>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav>
