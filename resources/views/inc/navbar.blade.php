66d2732e5729500a9f31ed86eb11c6772b7404ce
ChathuriChandima <chandimaracc@gmail.com>
2018-09-16 16:04:03 +0530

    service page and few changes of other pages

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,11 +1,11 @@
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+<nav class="navbar navbar-expand-sm bg-transparent navbar-dark fixed-top" >
         <!-- Brand/logo -->
-        <a class="navbar-brand" >
-          <img src="img/ic.png" alt="logo" style="width:50px;">
+        <a class="navbar-brand" href="/">
+          <img src="img/ic.png" alt="logo" style="width:70px;">
         </a>
         
         <!-- Links -->
-        <ul class="navbar-nav">
+        <ul class="navbar-nav" style="font-size:larger">
           <li class="nav-item">
             <a class="nav-link" href="/">Home</a>
           </li>
@@ -16,9 +16,9 @@
             <a class="nav-link" href="/contact">Contact Us</a>
           </li>
         </ul>
-        <ul class="navbar-nav ml-auto">
+        <ul class="navbar-nav ml-auto" style="font-size:larger">
                 <li class="nav-item">
-                    <a class="nav-link" href="/login"><img src="img\icons8_Login_35px.png" >Login</a>
+                    <a class="nav-link" href="/login" ><img src="img\icons8_Login_35px.png" >Login</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#"><img src="img\icons8_Add_User_Male_35px.png" >SignUp</a>
--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,33 +1,25 @@
 @extends('layouts.app')
 <link rel="stylesheet" href="{{asset('my/l.css')}}">
 @section('content')
-<br><br>
-<div class="container">
-        <div class="login-form">
-        <div class="main-div">
-            <div class="panel">
+<br><br><br><br><br>
+    <div class="container">
+    <div class="login-form">
+    <div class="main-div">
+        <div class="panel">
            <h1>Login</h1>
            <p>Please enter your username and password</p>
            </div>
             <form id="Login">
-        
                 <div class="form-group">
-        
-        
                     <input type="text" class="form-control" id="inputUsername" placeholder="Username">
-        
                 </div>
-        
                 <div class="form-group">
-        
                     <input type="password" class="form-control" id="inputPassword" placeholder="Password">
-        
                 </div>
                 <div class="forgot">
                 <a href="/reset">Forgot password?</a>
-        </div>
+                </div>
                 <button type="submit" class="btn btn-primary">Login</button>
-        
             </form>
             </div>
         </div></div>
