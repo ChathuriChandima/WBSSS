bc1a9c7fe930466fc2ec0eba28b400707aeaff56
Rishi0721 <rishmikalavandi@gmail.com>
2018-09-17 15:11:43 +0530

    database

---
+++ database/migrations/2018_09_17_093311_create_stocks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->integer('code')->primary;
+            $table->string('name');
+            $table->string('type');
+            $table->integer('availableStock');
+            $table->integer('purchasedStock');
+            $table->integer('soldStock');
+            $table->decimal('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+}
