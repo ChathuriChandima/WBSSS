285c37c96d872461cfcbb241eacd849b692ce1bf
Thiranja <lakrandikathiranja@gmail.com>
2018-09-19 03:17:53 +0530

    Reterning user's error of lost navigation to the home and other important links has been solved with some dirty codes and need attention to keep code dry and readable

--- resources/views/layouts/log.blade.php
+++ resources/views/layouts/log.blade.php
@@ -27,63 +27,18 @@
 </head>
 <body>
     <div id="app">
-      <nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top" >
-          <div class="container">
-            <!-- Brand/logo -->
-            <a class="navbar-brand" href="/">
-              <img src="img/ic.png" alt="logo" style="width:70px;">
-            </a>
-
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                  <span class="navbar-toggler-icon"></span>
-              </button>
-
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                  <!-- Left Side Of Navbar -->
-                  <ul class="navbar-nav" style="font-size:larger">
-                    <li class="nav-item">
-                      <a class="nav-link" href="/home">Home</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="">Profile</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" >Status</a>
-                    </li>
-                  </ul>
-
-                  <!-- Right Side Of Navbar -->
-                  <ul class="navbar-nav ml-auto">
-                      <!-- Authentication Links -->
-                          <li class="nav-item dropdown">
-                              <a id="navbarDropdown" class="nav-link dropdown-toggle" style="font-size:large" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                  {{ Auth::user()->name }} <span class="caret"></span>
-                              </a>
-                              <div class="card-body">
-                              <div class="dropdown-menu dropdown-menu-right bg-light" aria-labelledby="navbarDropdown">
-                                  <a class="dropdown-item bg-light" href="{{ route('logout') }}"
-                                     onclick="event.preventDefault();
-                                                   document.getElementById('logout-form').submit();">
-                                      {{ __('Logout') }}
-                                  </a>
-                              </div>
-
-                                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                      @csrf
-                                  </form>
-                              </div>
-                          </li>
-                  </ul>
-              </div>
-          </div>
-      </nav>
+        @guest
+            @include('elements.guestNavbar')
+        @else
+            @include('elements.logedNavbar')
+        @endguest
         <main class="py-4">
           <div>
               @include('elements.sidebar')
           </div>
           <div id="wrapper">
               @yield('content')
-        </div>
+          </div>
         </main>
     </div>
 </body>
