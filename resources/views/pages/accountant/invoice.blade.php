3943b12b1e5419f94b45ea9d264bb476bbe2504f
Rishi0721 <rishmikalavandi@gmail.com>
2018-09-30 04:20:16 +0530

    few changes to accountant panel

--- resources/views/pages/accountant/invoice.blade.php
+++ resources/views/pages/accountant/invoice.blade.php
@@ -1,7 +1,2 @@
 
 <img src="img/p1.jpg" width="1400px" height="300px">
-    <img src="img/ic.png" width="70px" >
-      <h1><img src="img/contact.gif" width="200px" ></h1>
-    <br><br><br>
-    <img src="img/email.jpg" width="60px" >
-    
