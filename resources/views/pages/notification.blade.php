9852a3853cbcf19165ad32e62f0e3905617f3d9b
thiranja <lakrandikathiranja@gmail.com>
2018-12-24 11:20:04 +0530

    notification view

--- resources/views/pages/notification.blade.php
+++ resources/views/pages/notification.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.log')
+
 <link rel="stylesheet" href="{{asset('my/m.css')}}">
 
 <div class="carousel-inner">
@@ -6,12 +7,23 @@
       <img src="img/p2.jpg" width="1600" height="300"style = "padding-left:250px">
       <div class="carousel-caption">
               <h1>RAJAAN MOTORS</h1>
-              <h2>Customer Details</h2>
+              <h2>Vehicle Details</h2>
 
       </div>
 </div>
-
 @section('content')
-<p>Here comes the customer page</p>
+
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+            <a href="add_vehicle" class="btn float-right"  title="New Vehicle"> <img src="img\icons8_Add_New_50px_1.png"  /></a>
+    </div>
+</div>
+@section('content')
+
+
+
+<p>Notification and All other setings about notification comes here</p>
+
+
 
 @endsection
