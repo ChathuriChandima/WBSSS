faa1bc04a8ca67d5cd7cef4a86cc351fc8a8db79
Rishi0721 <rishmikalavandi@gmail.com>
2019-01-10 05:57:39 +0530

    bill

--- resources/views/pages/bill/addBill.blade.php
+++ resources/views/pages/bill/addBill.blade.php
@@ -6,6 +6,7 @@
   }
 </style>
 @section('content')
+<!-- Issue a new bill-->
     <h2 style="margin-top:-30px;"><img src="img\bill.png">  New Bill </h2>
     <br>
     <div class="container">
@@ -25,75 +26,73 @@
             <div class="col-md-4 col-form-label text-md-right">
               <strong>{{Form::label('date','Billed Date')}}  :</strong>
             </div>
-                                <div class="col-md-6">
-                                {{Form::text('date',date('m/d/Y'),['class'=>'form-control','placeholder'=>'','id'=>'datepicker'])}}
-                                </div>
-                                <script>
-                                    $('#datepicker').datepicker();
-                                    </script>
-                              </div>
-                              </div>
-                              <div class="form-group">
-                                    <div class="row">
-                                    <div class="col-md-4 col-form-label text-md-right">
-                                    <strong>{{Form::label('customerName','Customer Name')}}  :</strong>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <input list="c" name="customerName" class="form-control">
-                                        <datalist id="c">
-                                            @foreach($customer as $c)
-                                        <option value="{{$c->name}}">
-                                            @endforeach
-                                        </datalist>
-                                    </div>
-                                  </div>
-                                  </div> 
-                                  <div class="form-group">
-                                        <div class="row">
-                                        <div class="col-md-4 col-form-label text-md-right">
-                                        <strong>{{Form::label('vehicleNo','Vehicle No')}}  :</strong>
-                                        </div>
-                                        <div class="col-md-6">
-                                        <input list="v" name="vehicleNo" class="form-control">
-                                          <datalist id="v">
-                                              @foreach($vehicle as $v)
-                                          <option value="{{$v->vehicleNo}}">
-                                              @endforeach
-                                          </datalist>
-                                        </div>
-                                      </div>
-                                      </div>
-                                <div class="form-group">
-                                        <div class="row">
-                                        <div class="col-md-4 col-form-label text-md-right">
-                                        <strong>{{Form::label('totalAmount','Total Amount')}}  :</strong>
-                                        </div>
-                                        <div class="col-md-6">
-                                        {{Form::number('totalAmount','',['class'=>'form-control','placeholder'=>''])}}
-                                        </div>
-                                      </div>
-                                      </div> 
-                                      <div class="form-group">
-                                            <div class="row">
-                                            <div class="col-md-4 col-form-label text-md-right">
-                                            <strong>{{Form::label('discount','Discount')}}  :</strong>
-                                            </div>
-                                            <div class="col-md-6">
-                                            {{Form::number('discount','',['class'=>'form-control','placeholder'=>''])}}
-                                            </div>
-                                          </div>
-                                          </div> 
-
-
-                                <div class="form-group float-right form-inline" style="margin-right:180px">
-                                <div class="form-group">
-
-                                {{Form::submit('Add',['class'=>'btn btn-success'] )}}
-                                </div>
-                                <div class="form-group">
-                                    <a href="/bills" class="btn btn-danger float-right " style="margin:5px"  id="cl" ><strong>Cancel</strong></a>
-                                  </div>
-                                </div>
-                          {!! Form::close() !!}
+            <div class="col-md-6">
+              {{Form::text('date',date('m/d/Y'),['class'=>'form-control','placeholder'=>'','id'=>'datepicker'])}}
             </div>
+            <script>
+                $('#datepicker').datepicker();
+            </script>
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="row">
+              <div class="col-md-4 col-form-label text-md-right">
+                <strong>{{Form::label('customerName','Customer Name')}}  :</strong>
+              </div>
+              <div class="col-md-6">
+                <input list="c" name="customerName" class="form-control">
+                <datalist id="c">
+                @foreach($customer as $c)
+                  <option value="{{$c->name}}">
+                @endforeach
+                </datalist>
+              </div>
+          </div>
+        </div> 
+        <div class="form-group">
+          <div class="row">
+            <div class="col-md-4 col-form-label text-md-right">
+              <strong>{{Form::label('vehicleNo','Vehicle No')}}  :</strong>
+            </div>
+            <div class="col-md-6">
+              <input list="v" name="vehicleNo" class="form-control">
+                <datalist id="v">
+                  @foreach($vehicle as $v)
+                    <option value="{{$v->vehicleNo}}">
+                  @endforeach
+                </datalist>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="row">
+            <div class="col-md-4 col-form-label text-md-right">
+              <strong>{{Form::label('totalAmount','Total Amount')}}  :</strong>
+            </div>
+            <div class="col-md-6">
+              {{Form::number('totalAmount','',['class'=>'form-control','placeholder'=>''])}}
+            </div>
+          </div>
+        </div> 
+        <div class="form-group">
+          <div class="row">
+            <div class="col-md-4 col-form-label text-md-right">
+              <strong>{{Form::label('discount','Discount')}}  :</strong>
+            </div>
+            <div class="col-md-6">
+              {{Form::number('discount','',['class'=>'form-control','placeholder'=>''])}}
+            </div>
+          </div>
+        </div> 
+        
+        <div class="form-group float-right form-inline" style="margin-right:180px">
+          <div class="form-group">
+            {{Form::submit('Add',['class'=>'btn btn-success'] )}}
+          </div>
+          <div class="form-group">
+            <a href="/bills" class="btn btn-danger float-right " style="margin:5px"  id="cl" ><strong>Cancel</strong></a>
+          </div>
+        </div>
+        {!! Form::close() !!}
+  </div>
 @endsection
--- resources/views/pages/bill/printbill.blade.php
+++ resources/views/pages/bill/printbill.blade.php
@@ -13,148 +13,138 @@ function printlayer(Layer){
 }
 </script>
 <div class="container">
-            <a href="/bills" class="btn btn-info float-left" > Back</a>
-   <a href="#" class="btn btn-success float-right" id="print" onclick="javascript:printlayer('c')">Download</a>
-  <div class="well" id="c">
-
-      {!! Form::open(['action'=>['billController@downloadPdf',$bill->billNo]]) !!}
-      <div class="form-group">
-            <div class="row">
-            <div class="col-md-6 col-form-label text-md-left">
-                    <h1 ><img src="\img\ic.png"> Rajaan Motors </h1>
+      <a href="/bills" class="btn btn-info float-left" > Back</a>
+      <a href="#" class="btn btn-success float-right" id="print" onclick="javascript:printlayer('c')">Download</a>
+      <div class="well" id="c">
+            {!! Form::open(['action'=>['billController@downloadPdf',$bill->billNo]]) !!}
+            <div class="form-group">
+                  <div class="row">
+                        <div class="col-md-6 col-form-label text-md-left">
+                              <h1 ><img src="\img\ic.png"> Rajaan Motors </h1>
+                        </div>
+                        <div class="col-md-1 col-form-label text-md-left">
+                              <strong>Phone:</strong><br>
+                              <strong>Email:</strong>
+                        </div>
+                        <div class="col-md-2 col-form-label text-md-left">
+                              <a href="tel:033-22-77616">033-22-77616</a><br>
+                              <a href="https://accounts.google.com/ServiceLogin/signinchooser?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F%3Frli%3Dn4mf18sk0chx%26rld%3D1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin">raajan@gmail.com</a><br>
+                              <br>
+                        </div>
+                        <div class="col-md-3 ">
+                        </div>
                   </div>
-                  <div class="col-md-1 col-form-label text-md-left">
-                        <strong>Phone:</strong><br>
-                        <strong>Email:</strong>
-                    </div>
-                    <div class="col-md-2 col-form-label text-md-left">
-                         <a href="tel:033-22-77616">033-22-77616</a><br>
-                         <a href="https://accounts.google.com/ServiceLogin/signinchooser?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F%3Frli%3Dn4mf18sk0chx%26rld%3D1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin">raajan@gmail.com</a><br>
-  <br>
-                  </div>
-                  <div class="col-md-3 ">
-                    </div>
-          </div>
-        </div>
-      <div class="form-group">
-            <div class="row">
-            <div class="col-md-3 col-form-label text-md-left">
-              <strong>{{Form::label('billNo','Bill No')}} :</strong>
-                </div>
-              <div class="col-md-2 col-form-label text-md-left">
-                {{Form::label('billNo',$bill->billNo)}}
-              </div><div class="col-md-2 col-form-label text-md-left">
-                    <strong>{{Form::label('date','Billed Date')}}  :</strong>
-                    </div>
-                    <div class="col-md-2 col-form-label text-md-left">
-                    {{Form::label('date',$bill->date)}}
-                    </div>
-
-            </div>
             </div>
             <div class="form-group">
-                    <div class="row">
-                    <div class="col-md-3 col-form-label text-md-left">
-                      <strong>{{Form::label('customerName','Customer Name')}} :</strong>
+                  <div class="row">
+                        <div class="col-md-3 col-form-label text-md-left">
+                              <strong>{{Form::label('billNo','Bill No')}} :</strong>
                         </div>
-                      <div class="col-md-2 col-form-label text-md-left">
-                        {{Form::label('customerName',$bill->customerName)}}
-                      </div><div class="col-md-2 col-form-label text-md-left">
-                            <strong>{{Form::label('vehicleNo','Vehicle No')}}  :</strong>
-                            </div>
-                            <div class="col-md-2 col-form-label text-md-left">
-                            {{Form::label('vehicleNo',$bill->vehicleNo)}}
-                            </div>
-        
-                    </div>
-                    </div>
-                    <div class="form-group">
-                            <div class="row">
-                            <div class="col-md-5 ">
-                                  
-                                  </div>
-                                  <div class="col-md-4 col-form-label text-md-left">
-                                  **********************************************
-                                  </div>
-                          </div>
-                          </div>
-                    <div class="form-group">
-                              <div class="form-group">
-                                    <div class="row">
-                                            <div class="col-md-5">
-                                                    
-                                                    </div>
-                                    <div class="col-md-2 col-form-label text-md-left">
-                                          <strong>{{Form::label('discount','Discount')}}  :</strong>
-                                          </div>
-                                          <div class="col-md-2 col-form-label text-md-left">
-                                          {{Form::label('discount',$bill->discount)}}
-                                          </div>
-                                  </div>
-                                  </div>
-                                  <div class="form-group">
-                                        <div class="row">
-                                        <div class="col-md-5 ">
-                                              
-                                              </div>
-                                              <div class="col-md-4 col-form-label text-md-left">
-                                              **********************************************
-                                              </div>
-                                      </div>
-                                      </div>
-                                  <div class="form-group">
-                                        <div class="row">
-                                                <div class="col-md-5 ">
-                                                        
-                                                        </div>
-                                        <div class="col-md-2 col-form-label text-md-left">
-                                              <strong>{{Form::label('totalAmount','Total Charges')}}  :</strong>
-                                              </div>
-                                              <div class="col-md-2 col-form-label text-md-left">
-                                              {{Form::label('totalAmount',$bill->totalAmount)}}
-                                              </div>
-                                      </div>
-                                      </div>
-                                      <div class="form-group">
-                                            <div class="row">
-                                            <div class="col-md-5 ">
-                                                  
-                                                  </div>
-                                                  <div class="col-md-4 col-form-label text-md-left">
-                                                  **********************************************
-                                                  </div>
-                                          </div>
-                                          </div>
-                                          <div class="form-group">
-                                                <div class="row">
-                                                <div class="col-md-5 col-form-label text-md-right">
-                                                      
-                                                      </div>
-                                                      <div class="col-md-4 col-form-label text-md-left">
-                                                      **********************************************
-                                                      </div>
-                                              </div>
-                                              </div>
-             
-              </div>
-              <div class="form-group">
-                    <div class="row">
-                    <div class="col-md-3 ">
-                          </div>
-                          <div class="col-md-3 col-form-label text-md-center">
+                        <div class="col-md-2 col-form-label text-md-left">
+                              {{Form::label('billNo',$bill->billNo)}}
+                        </div>
+                        <div class="col-md-2 col-form-label text-md-left">
+                              <strong>{{Form::label('date','Billed Date')}}  :</strong>
+                        </div>
+                        <div class="col-md-2 col-form-label text-md-left">
+                              {{Form::label('date',$bill->date)}}
+                        </div>
+                  </div>
+            </div>
+            <div class="form-group">
+                  <div class="row">
+                        <div class="col-md-3 col-form-label text-md-left">
+                              <strong>{{Form::label('customerName','Customer Name')}} :</strong>
+                        </div>
+                        <div class="col-md-2 col-form-label text-md-left">
+                              {{Form::label('customerName',$bill->customerName)}}
+                        </div>
+                        <div class="col-md-2 col-form-label text-md-left">
+                              <strong>{{Form::label('vehicleNo','Vehicle No')}}  :</strong>
+                        </div>
+                        <div class="col-md-2 col-form-label text-md-left">
+                              {{Form::label('vehicleNo',$bill->vehicleNo)}}
+                        </div>
+                  </div>
+            </div>
+            <div class="form-group">
+                  <div class="row">
+                        <div class="col-md-5 ">
+                        </div>
+                        <div class="col-md-4 col-form-label text-md-left">
+                              **********************************************
+                        </div>
+                  </div>
+            </div>
+            <div class="form-group">
+                  <div class="form-group">
+                        <div class="row">
+                              <div class="col-md-5">                                              
+                              </div>
+                              <div class="col-md-2 col-form-label text-md-left">
+                                    <strong>{{Form::label('discount','Discount')}}  :</strong>
+                              </div>
+                              <div class="col-md-2 col-form-label text-md-left">
+                                    {{Form::label('discount',$bill->discount)}}
+                        </div>
+                  </div>
+            </div>
+            <div class="form-group">
+                  <div class="row">
+                        <div class="col-md-5 ">                      
+                        </div>
+                        <div class="col-md-4 col-form-label text-md-left">
+                              **********************************************
+                        </div>
+                  </div>
+            </div>
+            <div class="form-group">
+                  <div class="row">
+                        <div class="col-md-5 ">                               
+                        </div>
+                        <div class="col-md-2 col-form-label text-md-left">
+                              <strong>{{Form::label('totalAmount','Total Charges')}}  :</strong>
+                        </div>
+                        <div class="col-md-2 col-form-label text-md-left">
+                              {{Form::label('totalAmount',$bill->totalAmount)}}
+                        </div>
+                  </div>
+            </div>
+            <div class="form-group">
+                  <div class="row">
+                        <div class="col-md-5 ">                          
+                        </div>
+                        <div class="col-md-4 col-form-label text-md-left">
+                              **********************************************
+                        </div>
+                  </div>
+            </div>
+            <div class="form-group">
+                  <div class="row">
+                        <div class="col-md-5 col-form-label text-md-right">                              
+                        </div>
+                        <div class="col-md-4 col-form-label text-md-left">
+                              **********************************************
+                        </div>
+                  </div>
+            </div>
+            <div class="form-group">
+                  <div class="row">
+                        <div class="col-md-3 ">
+                        </div>
+                        <div class="col-md-3 col-form-label text-md-center">
                               <strong>Thank You  !!!</strong><br><br>
-                                <address>
+                                    <address>
                                         No.210/5 <br>
                                         Weerangala South<br>
                                         Yakkala 
                                     </address>
-                          </div>
-                          <div class="col-md-3 ">
-                            </div>
+                        </div>
+                        <div class="col-md-3 ">
+                        </div>
                   </div>
-                  </div>
-              
-      {!! Form::close() !!}
-  </div>
+            </div>  
+                  {!! Form::close() !!}
+      </div>
 </div>  
 @endsection
